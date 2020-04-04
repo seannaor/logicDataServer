3e03cb4cc787b538e4ad8cfd223e8545dc936989
Gilad Chen <giladchen2341@gmail.com>
2020-04-04 18:32:30 +0300

    routing layer
    divided to the three entity

---
+++ src/main/java/com/example/demo/RoutingLayer/GraderRouter.java
@@ -0,0 +1,39 @@
+package com.example.demo.RoutingLayer;
+
+import com.example.demo.BusinessLayer.Entities.Stages.Stage;
+import com.example.demo.ServiceLayer.CreatorService;
+import com.example.demo.ServiceLayer.ExperimenteeService;
+import com.example.demo.ServiceLayer.GraderService;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.example.demo.RoutingLayer.RouterUtils.strToJSON;
+
+@RestController
+@RequestMapping("/")
+public class GraderRouter {
+
+    private GraderService grader = new GraderService();
+
+//    private Map<String, IService> serviceMap = new HashMap<String, IService>() {{
+//        put("manager", new CreatorService());
+//        put("grader", new GraderService());
+//        put("experimentee", new ExperimenteeService());
+//    }};
+
+    @RequestMapping("/grader")
+    public Map<String, Object> graderLogin(@RequestParam String code) {
+        return grader.beginGrading(code);
+    }
+
+}
+
+
