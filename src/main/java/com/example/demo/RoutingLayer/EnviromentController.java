a3371845f1f4334d0e2e74728fee284cd322b695
Shahar <shaharmorad13239@gmail.com>
2020-07-05 18:05:03 +0300

    http requests

--- src/main/java/com/example/demo/RoutingLayer/EnviromentController.java
+++ src/main/java/com/example/demo/RoutingLayer/EnviromentController.java
@@ -1,10 +1,16 @@
 package com.example.demo.RoutingLayer;
 
 import com.example.demo.ServiceLayer.ExperimenteeService;
+import org.asynchttpclient.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
+import java.net.http.HttpHeaders;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
 
 
 @RestController
@@ -12,10 +18,21 @@ import java.util.Map;
 public class EnviromentController {
 
     private ExperimenteeService expee;
+    private final RestTemplate restTemplate;
+    private final String tokenUrl = "https://judge0.p.rapidapi.com/";
+    private final String judge0Key = "460509d2cemsh25d3565f9399f0bp1004c7jsne3f4ac235341";
+    private HttpHeaders headers;
+    AsyncHttpClient client = Dsl.asyncHttpClient();
+    DefaultAsyncHttpClientConfig.Builder clientBuilder = Dsl.config();
+//    = new HttpHeaders({
+//        "X-RapidAPI-Host": "judge0.p.rapidapi.com",
+//                "X-RapidAPI-Key": judge0Key
+//    });
 
     @Autowired
-    public EnviromentController(ExperimenteeService expee) {
+    public EnviromentController(ExperimenteeService expee, RestTemplateBuilder restTemplateBuilder) {
         this.expee = expee;
+        this.restTemplate = restTemplateBuilder.build();
     }
 
     @PostMapping("/login")
@@ -35,6 +52,32 @@ public class EnviromentController {
                                            @RequestBody Map<String, Object> stageInfo) {
         System.out.println("/submitStage " + accessCode + " data " + stageInfo);
         return expee.fillInStage(accessCode, stageInfo);
+    }
+
+    @GetMapping("/getLanguages")
+    public String getLanguages() {
+        String url = tokenUrl + "languages";
+        Request request = Dsl.get(url).build();
+
+        ListenableFuture<Response> listenableFuture = client
+                .executeRequest(request);
+        listenableFuture.addListener(() -> {
+            try {
+                Response response = listenableFuture.get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }, Executors.newCachedThreadPool());
+
+        try {
+            Response response = listenableFuture.get();
+            return response.getContentType() + '\n' + response.getResponseBody() + '\n' + response.getStatusText();
+        } catch (InterruptedException | ExecutionException ignore) {
+        }
+        return "Oops, something went wrong...";
+//        return Map.of("Error","Oops, something went wrong...");
     }
 
 //    @RequestMapping("/next_stage")
